5d9fae52e5939bb5e66cfcbbb5367fe59af68bf6
Braejan Arias <ing.brayanarias@gmail.com>
2025-03-02 10:47:37 -0500

    :chores: add modification.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from infrastructure.api.fastapi.routes.routes import register_routes
+from src.infrastructure.api.fastapi.routes.routes import register_routes
 
 def create_app() -> FastAPI:
     """
@@ -37,7 +37,7 @@ app = create_app()
 if __name__ == "__main__":
     # Run the application with Uvicorn
     uvicorn.run(
-        "src.main:app",
+        "main:app",
         host="0.0.0.0",
         port=8000,
         reload=True,  # Enable auto-reload during development
